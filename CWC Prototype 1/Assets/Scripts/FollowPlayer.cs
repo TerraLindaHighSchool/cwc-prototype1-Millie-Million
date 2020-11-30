fa8d23a1219109565d95a98129c2c375383b6835
Millie <47400469+Minnie-Million@users.noreply.github.com>
2020-11-30 10:04:06 -0800

    Tutorial 1.3.5 Complete

--- CWC Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ CWC Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -10);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,6 @@ public class FollowPlayer : MonoBehaviour
     void Update()
     {
         //Offset the camera's position by adding to the player's position.
-        transform.position = player.transform.position + new Vector3(0, 5, -10);
+        transform.position = player.transform.position + offset;
     }
 }
